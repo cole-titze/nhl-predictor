ce3d095760f41c4f6a0ebb2cd49b6bdc188fd2c8
Cole Titze <cole.titze@outlook.com>
2022-01-02 15:25:44 -0600

    All columns finished. Tweaking needed for home and away avg goals

--- DataRetrieval/get_pregame_data.py
+++ DataRetrieval/get_pregame_data.py
@@ -3,12 +3,12 @@ import csv
 # TODO: Make list of years something passed in instead of magic string
 
 
-def sort(pregame_data):
+def sort(pregame_data: list) -> list:
     pregame_data.sort(key=lambda x: x[1])
     return pregame_data
 
 
-def get_played_games(game_id: int, season_data: list):
+def get_played_games(game_id: int, season_data: list) -> list:
     valid_games = []
     for row in season_data:
         if int(row[0]) < game_id:
@@ -16,7 +16,7 @@ def get_played_games(game_id: int, season_data: list):
     return valid_games
 
 
-def get_games_by_team(valid_games: list, team_name: str):
+def get_games_by_team(valid_games: list, team_name: str) -> list:
     team_games = []
     for row in valid_games:
         if row[1] == team_name or row[2] == team_name:
@@ -24,7 +24,7 @@ def get_games_by_team(valid_games: list, team_name: str):
     return team_games
 
 
-def game_win(game: list, team_name: str):
+def game_win(game: list, team_name: str) -> bool:
     if game[1] == team_name and game[7] == "Home":
         return True
     if game[2] == team_name and game[7] == "Away":
@@ -32,13 +32,40 @@ def game_win(game: list, team_name: str):
     return False
 
 
-def game_draw(game: list):
+def game_loss(game: list, team_name: str) -> bool:
+    if game[1] == team_name and game[7] == "Away":
+        return True
+    if game[2] == team_name and game[7] == "Home":
+        return True
+    return False
+
+
+def game_draw(game: list) -> bool:
     if game[7] == "Draw":
         return True
     return False
 
 
-def get_draw_ratio(game_id: int, season_data: list, team_name: str):
+def get_loss_ratio(game_id: int, season_data: list, team_name: str) -> float:
+    loss_ratio = 0
+    valid_games = get_played_games(game_id, season_data)
+    team_games = get_games_by_team(valid_games, team_name)
+
+    # Reverse to walk through previous games
+    team_games.reverse()
+    count = 0
+    for row in team_games:
+        if count == 5:
+            break
+        if game_loss(row, team_name):
+            loss_ratio = loss_ratio + 1
+        count = count + 1
+    if count > 0:
+        loss_ratio = float(loss_ratio) / float(count)
+    return loss_ratio
+
+
+def get_draw_ratio(game_id: int, season_data: list, team_name: str) -> float:
     draw_ratio = 0
     valid_games = get_played_games(game_id, season_data)
     team_games = get_games_by_team(valid_games, team_name)
@@ -57,7 +84,7 @@ def get_draw_ratio(game_id: int, season_data: list, team_name: str):
     return draw_ratio
 
 
-def get_win_ratio(game_id: int, season_data: list, team_name: str):
+def get_win_ratio(game_id: int, season_data: list, team_name: str) -> float:
     win_ratio = 0
     valid_games = get_played_games(game_id, season_data)
     team_games = get_games_by_team(valid_games, team_name)
@@ -76,12 +103,110 @@ def get_win_ratio(game_id: int, season_data: list, team_name: str):
     return win_ratio
 
 
-def get_single_season(historic_data: list, season: str):
+def get_single_season(historic_data: list, season: str) -> list:
     season_data = []
     for row in historic_data:
         if row[3] == season:
             season_data.append(row)
     return season_data
+
+
+def get_head_to_head_ratio(game_id: int, season_data: list, team_one: str, team_two: str):
+    # Get games that both teams played in
+    valid_games = get_played_games(game_id, season_data)
+    team_games = get_games_by_team(valid_games, team_one)
+    head_to_head_games = get_games_by_team(team_games, team_two)
+
+    # Count games won and divide by total games
+    win_ratio = 0
+    count = 0
+    for game in head_to_head_games:
+        if game_win(game, team_one):
+            win_ratio = win_ratio + 1
+        count = count + 1
+    if count > 0:
+        win_ratio = float(win_ratio) / float(count)
+    return win_ratio
+
+
+def get_home_goals(game: list, team_name: str) -> int:
+    if game[1] == team_name:
+        return int(game[5])
+    return 0
+
+
+def get_away_goals(game: list, team_name: str) -> int:
+    if game[2] == team_name:
+        return int(game[6])
+    return 0
+
+
+def get_goals(game: list, team_name: str) -> int:
+    goals = 0
+    goals += get_away_goals(game, team_name)
+    goals += get_home_goals(game, team_name)
+
+    return goals
+
+
+def get_current_goals_per_game(game_id: int, season_data: list, team_name: str) -> float:
+    valid_games = get_played_games(game_id, season_data)
+    team_games = get_games_by_team(valid_games, team_name)
+
+    goals_avg = 0
+    count = 0
+    for game in team_games:
+        goals_avg = goals_avg + get_goals(game, team_name)
+        count = count + 1
+    if count > 0:
+        goals_avg = goals_avg / count
+    return goals_avg
+
+
+def get_home_games(team_games: list, team_name: str) -> list:
+    home_games = []
+    for game in team_games:
+        if game[1] == team_name:
+            home_games.append(game)
+    return home_games
+
+
+def get_away_games(team_games: list, team_name: str) -> list:
+    away_games = []
+    for game in team_games:
+        if game[1] == team_name:
+            away_games.append(game)
+    return away_games
+
+
+def get_current_goals_per_game_home(game_id: int, season_data: list, team_name: str) -> float:
+    valid_games = get_played_games(game_id, season_data)
+    team_games = get_games_by_team(valid_games, team_name)
+    home_games = get_home_games(team_games, team_name)
+
+    goals_avg = 0
+    count = 0
+    for game in home_games:
+        goals_avg += get_home_goals(game, team_name)
+        count = count + 1
+    if count > 0:
+        goals_avg = goals_avg / count
+    return goals_avg
+
+
+def get_current_goals_per_game_away(game_id: int, season_data: list, team_name: str) -> float:
+    valid_games = get_played_games(game_id, season_data)
+    team_games = get_games_by_team(valid_games, team_name)
+
+    goals_avg = 0
+    count = 0
+    for game in team_games:
+        goals_avg += get_away_goals(game, team_name)
+        count = count + 1
+    if count > 0:
+        goals_avg = goals_avg / count
+    return goals_avg
+
 
 
 def get_pregame_statistics():
@@ -100,20 +225,20 @@ def get_pregame_statistics():
                     home_team,
                     get_win_ratio(game_id, season_data, home_team),
                     get_draw_ratio(game_id, season_data, home_team),
-                    # get_loss_ratio(),
-                    # get_head_to_head_ratio(),
-                    # get_current_goals_per_game(),
-                    # get_current_goals_per_game_home_away()
+                    get_loss_ratio(game_id, season_data, home_team),
+                    get_head_to_head_ratio(game_id, season_data, home_team, away_team),
+                    get_current_goals_per_game(game_id, season_data, home_team),
+                    get_current_goals_per_game_home(game_id, season_data, home_team)
                 ]
                 pregame_row_away = [
                     game_id,
                     away_team,
                     get_win_ratio(game_id, season_data, away_team),
                     get_draw_ratio(game_id, season_data, away_team),
-                    # get_loss_ratio(),
-                    # get_head_to_head_ratio(),
-                    # get_current_goals_per_game(),
-                    # get_current_goals_per_game_home_away()
+                    get_loss_ratio(game_id, season_data, away_team),
+                    get_head_to_head_ratio(game_id, season_data, away_team, home_team),
+                    get_current_goals_per_game(game_id, season_data, away_team),
+                    get_current_goals_per_game_away(game_id, season_data, home_team)
                 ]
                 pregame_data.append(pregame_row_home)
                 pregame_data.append(pregame_row_away)
