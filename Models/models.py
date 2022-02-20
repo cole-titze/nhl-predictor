c2c6dec62031e1c842e5ade8a7defeb8cbcc4b12
Cole Titze <cole.titze@outlook.com>
2022-02-20 12:15:07 -0700

    Made the models file an object that holds all of the classifiers. Reworked Predicting to use the model object

--- Models/models.py
+++ Models/models.py
@@ -1,21 +1,59 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cluster import KMeans
 from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
 
-def train_forrest_classifier(x, y):
-    # Train Model: Fitting Random Forest Classification to the Training set
-    rf = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=42)
-    rf.fit(x, y)
+class Models:
+    neural_net = None
+    forrest = None
+    pca_kmeans = None
+    pca_neural_net = None
+    pca_nearest_neighbors = None
+    prediction = None
 
-    return rf
+    def __init__(self, x_training, y_training, x_testing, y_testing):
+        pca = PCA(n_components=2)
+        self.x_train = x_training
+        self.y_train = y_training
+        self.x_test = x_testing
+        self.y_test = y_testing
+        self.x_train_reduced = pca.fit_transform(self.x_train)
+        self.x_test_reduced = pca.fit_transform(self.x_test)
 
+    def train_models(self):
+        self.train_forrest_classifier()
+        self.train_neural_net()
+        self.train_pca_neural_net()
+        self.train_pca_nearest_neighbors()
+        self.train_pca_kmeans()
 
-def train_neural_net(x, y):
-    clf = MLPClassifier(max_iter=1000, solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-    clf.fit(x, y)
-    return clf
+    def train_forrest_classifier(self):
+        rf = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=0)
+        rf.fit(self.x_train, self.y_train)
+        self.forrest = rf
 
+    def train_neural_net(self):
+        clf = MLPClassifier(max_iter=1000, solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+        clf.fit(self.x_train, self.y_train)
+        self.neural_net = clf
 
-def train_kmeans(x, y):
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(x, y)
-    return kmeans
+    def train_pca_neural_net(self):
+        cnn = MLPClassifier(max_iter=1000, solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+        cnn.fit(self.x_train, self.y_train)
+        self.pca_neural_net = cnn
+
+    def train_pca_kmeans(self):
+        kmeans = KMeans(n_clusters=2, random_state=42)
+        kmeans.fit(self.x_train, self.y_train)
+        self.pca_kmeans = kmeans
+
+    def train_pca_nearest_neighbors(self):
+        nearest_neighbors = KNeighborsClassifier(n_neighbors=15)
+        nearest_neighbors.fit(self.x_train, self.y_train)
+        self.pca_nearest_neighbors = nearest_neighbors
+
+    def predict_results(self, model):
+        y_prediction = model.predict(self.x_test)
+        self.prediction = accuracy_score(self.y_test, y_prediction)
--- Predicting.py
+++ Predicting.py
@@ -1,12 +1,9 @@
-from sklearn.metrics import accuracy_score
-from sklearn.decomposition import PCA
-import Models.models as models
+import Models.models as math_models
 import DataRetrieval.get_prediction_data as data
 import Plotting.plotting as plt
 from sklearn.metrics import log_loss
 import DataRetrieval.teams as team
 
-teams = team.Teams
 x_training_headers = ['home_win_ratio_5', 'home_current_goals_avg', 'home_current_goals_avg',
                       'home_current_goals_avg_h', 'home_conceded_goals_avg', 'home_conceded_goals_avg_h',
                       'home_goal_average_5', 'home_conceded_average_5', 'h2h_w_d_l_ratio', 'away_win_ratio_5',
@@ -14,40 +11,42 @@ x_training_headers = ['home_win_ratio_5', 'home_current_goals_avg', 'home_curren
                       'away_conceded_goals_avg', 'away_conceded_goals_avg_h', 'away_goal_average_5',
                       'away_conceded_average_5']
 
-def predict_results(classifier, x, y):
-    y_prediction = classifier.predict(x)
-    print(accuracy_score(y, y_prediction))
-
 def get_models(x_train, y_train, x_test_season, y_test_season):
+    models = math_models.Models(x_train, y_train, x_test_season, y_test_season)
+
+    # Train Models
+    models.train_models()
+
     # Make predictions on original Dimensionality
     print("Forrest Prediction: ")
-    forrest_classifier = models.train_forrest_classifier(x_train, y_train)
-    predict_results(forrest_classifier, x_test_season, y_test_season)
+    models.predict_results(models.forrest)
+    print(models.prediction)
+
     print("Neural Network Prediction: ")
-    neural_net_classifier = models.train_neural_net(x_train, y_train)
-    predict_results(neural_net_classifier, x_test_season, y_test_season)
-    x_test_season_prob = neural_net_classifier.predict_proba(x_test_season)
+    models.predict_results(models.neural_net)
+    models.predict_results(models.neural_net)
+    print(models.prediction)
+    x_test_season_prob = models.neural_net.predict_proba(x_test_season)
     lg_log_loss = log_loss(y_test_season, x_test_season_prob)
     print("Neural Network Log Loss: " + str(lg_log_loss))
 
-    # Reduce Dimensionality
-    pca = PCA(n_components=2)
-    x_train_reduced = pca.fit_transform(x_train)
-    x_test_season_reduced = pca.fit_transform(x_test_season)
-
     # Plot lower dimensionality
-    # plt.plot_reduced_dimensionality(x_test_season_reduced, y_test_season)
+    # plt.plot_reduced_dimensionality(models.x_test_reduced, y_test)
 
     # Predict Reduced Dimensionality
     print("PCA K-Means Prediction: ")
-    kmeans = models.train_kmeans(x_train_reduced, y_train)
-    predict_results(kmeans, x_test_season_reduced, y_test_season)
+    models.predict_results(models.pca_kmeans)
+    print(models.prediction)
 
     print("PCA Neural Network Prediction: ")
-    net = models.train_neural_net(x_train_reduced, y_train)
-    predict_results(net, x_test_season_reduced, y_test_season)
+    models.predict_results(models.pca_neural_net)
+    print(models.prediction)
 
-    return forrest_classifier, neural_net_classifier, kmeans, net
+    print("PCA Nearest Neighbors Prediction: ")
+    models.predict_results(models.pca_nearest_neighbors)
+    print(models.prediction)
+
+    return models
 
 # Look into cleaning data and what is best for each predictor
 # Combine predictions (sensor fusion)
@@ -56,12 +55,16 @@ def get_models(x_train, y_train, x_test_season, y_test_season):
 def run_models(away_team: str, home_team: str):
     x_train, y_train, x_test_season, y_test_season = data.split_data()
 
-    forrest_classifier, neural_net_classifier, kmeans, net = get_models(x_train, y_train, x_test_season, y_test_season)
+    models = get_models(x_train, y_train, x_test_season, y_test_season)
 
     # Predict Current Season Games
     x_current_game = data.get_single_game(home_team, away_team)
     x_current_game = [x_current_game]
     print(away_team + "     " + home_team + " Percentage Chance")
-    print(neural_net_classifier.predict_proba(x_current_game))
+    print(models.neural_net.predict_proba(x_current_game))
 
-run_models(teams.Kraken, teams.Flames)
+def main():
+    teams = team.Teams()
+    run_models(teams.Kraken, teams.Flames)
+
+main()
